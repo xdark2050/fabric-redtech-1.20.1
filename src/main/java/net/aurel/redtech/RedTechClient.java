9b1ac409ebf6a70f084f897511b9340e4b754df4
Aurel <oorell44@gmail.com>
2024-08-22 15:49:44 +0200

    initial commit

---
+++ src/main/java/net/aurel/redtech/RedTechClient.java
@@ -0,0 +1,10 @@
+package net.aurel.redtech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedTechClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/aurel/redtech/RedTechDataGenerator.java
@@ -0,0 +1,11 @@
+package net.aurel.redtech;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RedTechDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
